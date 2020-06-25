cdfe3ba944052bf529a566adf1fa913993d4ab06
SantiagoPacheco1794 <santiago.pacheco.1794@gmail.com>
2020-06-25 16:43:26 -0300

    my firs commit

---
+++ src/main/java/Model/Table.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Corbenik
+ */
+public class Table {
+    private boolean reservada;
+    
+    public void setReservada(boolean reservada){
+        this.reservada = reservada;
+    }
+       
+    public boolean getReservada(){
+        return this.reservada;
+    }
+       
+    public boolean estaReservada(boolean reservada){
+        boolean result = true;
+        this.reservada = reservada;
+        if(reservada == result){
+            System.out.println("La mesa esta reservada");
+        }
+        else {
+            System.out.println("La mesa esta libre");
+            result = false;
+        }
+        return result;
+    }
+}
+
